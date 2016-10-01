eb1c74809ca6dd952563461976a728ee0abfee4a
Fonte Clanton <fonte@draco>
2016-10-01 11:48:22 -0400

    Update home variable reference

--- ignorectagfiles.sh
+++ ignorectagfiles.sh
@@ -3,5 +3,5 @@
 # Set git to ignore ctag's tag files by default
 # http://ricostacruz.com/til/navigate-code-with-ctags
 
-echo "tags" >> ~/.global_ignore
+echo "tags" >> $HOME/.global_ignore
 git config --global core.excludesfile $HOME/.global_ignore
